e220b039bb3b34e6ec35633562b4d72d8ea67a53
Maxim <vk_vm@ukr.net>
2019-09-09 23:02:36 +0300

    Revert "я все сломал блять"
    This reverts commit bdb21ccc28e912de1d8e26e62d3f20ae410481cf.

--- main.c
+++ main.c
@@ -32,50 +32,14 @@ void proceedEvent(XEvent event)
     }
 }
 
-unsigned int VBO, VAO;
+unsigned int VBO, VAO, EBO;
 
 float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f,   1.0f, 0.0f,
-        0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,   1.0f, 1.0f,
-        0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        // positions          // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
 };
 
 unsigned int indices[] = {  // note that we start from 0!
@@ -92,68 +56,33 @@ mat4 model;
 
 float angle = 0;
 
-mat4 xRotate;
-mat4 yRotate;
-mat4 zRotate;
-
-float positions[10][3] = {
-    {  0.0f,  0.0f,  0.0f },
-    {  2.0f,  5.0f, -15.0f },
-    { -1.5f, -2.2f, -2.5f },
-    { -3.8f, -2.0f, -12.3f },
-    {  2.4f, -0.4f, -3.5f },
-    { -1.7f,  3.0f, -7.5f },
-    {  1.3f, -2.0f, -2.5f },
-    {  1.5f,  2.0f, -2.5f },
-    {  1.5f,  0.2f, -1.5f },
-    { -1.3f,  1.0f, -1.5 },
-};
-
 void drawingRoutine()
 {
     // ------
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     // draw our first triangle
     sh_use(triangleSh);
     t_bind(tex);
 
+    identityMat(model);
+    rotateMat4X(model, angle += 0.01);
+
 
     sh_setMat4(triangleSh, "projection", projection);
     sh_setMat4(triangleSh, "view", view);
-
-    glBindVertexArray(VAO);
+    sh_setMat4(triangleSh, "model", model);
 
     sh_setInt(triangleSh, "img", 0);
 
-    identityMat(xRotate);
-    identityMat(yRotate);
-    identityMat(zRotate);
-
-    for(size_t i = 0; i < 10; i++)
-    {
-        identityMat(model);
-
-        rotateMat4X(xRotate, 5 * i);
-        rotateMat4Y(yRotate, 3 * i);
-        rotateMat4Z(zRotate, 0);
-
-        mat4_mulm(model, xRotate);
-        mat4_mulm(model, yRotate);
-        mat4_mulm(model, zRotate);
-
-        translateMat(model,
-                positions[i][0],
-                positions[i][1],
-                positions[i][2]);
-
-        sh_setMat4(triangleSh, "model", model);
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // glBindVertexArray(0); // no need to unbind it every time
 
     w_swapBuffers(win);
+
     glBindTexture(GL_TEXTURE_2D, 0);
     glUseProgram(0);
 }
@@ -162,11 +91,15 @@ void initTriangle()
 {
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -189,7 +122,7 @@ void initTriangle()
     sh_info(triangleSh);
     sh_setInt(triangleSh, "ourTexture", 0);
 
-    float angleOfView = 60;
+    float angleOfView = 90;
     float near = 0.1;
     float far = 100;
     float imageAspectRatio = win->w / (float)win->h;
@@ -206,16 +139,13 @@ void initTriangle()
     printMat4(projection);
     printMat4(view);
     printMat4(model);
-
-    xRotate = cmat4();
-    yRotate = cmat4();
-    zRotate = cmat4();
 }
 
 void freeTriangle(void)
 {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 }
 
 int main(int argc, char* argv[])
