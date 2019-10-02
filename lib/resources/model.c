0d777c4ef2ee6aa59f3325aca131559b6f304171
Maxim <vk_vm@ukr.net>
2019-10-02 15:23:46 +0300

    Model structure

--- lib/resources/model.c
+++ lib/resources/model.c
@@ -24,20 +24,36 @@ void push_texture()
 
 }
 
-const char* allowedDescriptors[] =
+typedef enum {
+    od_vertex,
+    od_vertexNormal,
+    od_vertexTex,
+    od_face,
+    od_group,
+    od_object,
+    od_mtllib,
+    od_usemtl,
+    od_comment
+
+} objDescriptorType_t;
+
+typedef struct {
+    const char* string;
+    objDescriptorType_t type;
+
+} descriptor_t;
+
+descriptor_t allowedDescriptors[] =
 {
-    "v",
-    "vn",
-    "vt",
-    "f",
-
-    "g",
-    "o",
-
-    "mtllib",
-    "usemtl",
-
-    "#",
+    { "v",      od_vertex },
+    { "vn",     od_vertexNormal },
+    { "vt",     od_vertexTex },
+    { "f",      od_face },
+    { "g",      od_group },
+    { "o",      od_object },
+    { "mtllib", od_mtllib },
+    { "usemtl", od_usemtl },
+    { "#",      od_comment }
 };
 
 bool isSeparator(char sym)
@@ -45,11 +61,129 @@ bool isSeparator(char sym)
     return (sym == ' ' || sym == '\n' || sym == '\t' || sym == '\0');
 }
 
-void readToSeparator(char* string, size_t position, size_t* resPos)
+#define MAX_WORD_LEN 256
+char word[MAX_WORD_LEN];
+const size_t descriptorsCount = sizeof(allowedDescriptors) / sizeof(allowedDescriptors[0]);
+
+
+
+bool readNextWord(char* string, size_t* startIndex, size_t endIndex)
 {
-    size_t strLen = strlen(string);
-    while(!isSeparator(string[position]) && position < strLen) position++;
-    *resPos = position;
+    if(*startIndex >= endIndex)
+        return false;
+
+    size_t start = *startIndex;
+
+    while(isSeparator(string[start])) start++;
+    *startIndex = start;
+
+    while(!isSeparator(string[*startIndex])) (*startIndex)++;
+
+    size_t len = *startIndex - start;
+    assert(len < MAX_WORD_LEN);
+
+    memset(word, 0, len + 1);
+    memcpy(word, string + start, len);
+    return true;
+}
+
+void proceedLine(char* string, size_t startIndex, size_t endIndex)
+{
+    bool firstWord = true;
+    descriptor_t* descriptor = NULL;
+
+    while((readNextWord(string, &startIndex, endIndex)))
+    {
+        printf("Word %s\n", word);
+        if(firstWord)
+        {
+            for (size_t i = 0; i < descriptorsCount; i++)
+                if (!strcmp(word, allowedDescriptors[i].string))
+                {
+                    descriptor = &allowedDescriptors[i];
+                }
+
+            if (descriptor == NULL)
+            {
+                printf("%s is unknown line descriptor", word);
+                exit(EXIT_FAILURE);
+            }
+
+            firstWord = false;
+            printf("Descriptor: %s", descriptor->string);
+        }
+        else
+        {
+            if (descriptor->type == od_comment)
+                continue;
+            //if(descriptor->type == od_vertex)
+
+        }
+    }
+}
+
+void getIndexes(char* str)
+{
+    size_t startIndex = 0;
+    size_t endIndex = 0;
+
+    size_t i = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] != '\n' && str[i] != '\0')
+        {
+            startIndex = i;
+            while(str[i] != '\n' && str[i] != '\0')
+                i++;
+            endIndex = i - 1;
+            proceedLine(str, startIndex, endIndex);
+        }
+        else i++;
+    }
+}
+
+model_t* m_create()
+{
+    model_t* model = malloc(sizeof(model_t));
+    model->modelLen = malloc(sizeof(modelLen_t));
+    model->modelLen->verticesMaxCount = START_LEN_COUNT;
+    model->modelLen->normalsMaxCount = START_LEN_COUNT;
+    model->modelLen->texCoordsMaxCount = START_LEN_COUNT;
+    model->modelLen->facesMaxCount = START_LEN_COUNT;
+    model->modelLen->groupNamesMaxCount = START_LEN_COUNT;
+    model->modelLen->mtlLibsMaxCount = START_LEN_COUNT;
+    model->modelLen->usedMaterialsMaxCount = START_LEN_COUNT;
+
+    model->modelLen->verticesCount = 0;
+    model->modelLen->normalsCount = 0;
+    model->modelLen->texCoordsCount = 0;
+    model->modelLen->facesCount = 0;
+    model->modelLen->groupNamesCount = 0;
+    model->modelLen->mtlLibsCount = 0;
+    model->modelLen->usedMaterialsCount = 0;
+
+    model->vertices = malloc(sizeof(vec4) * model->modelLen->verticesMaxCount);
+    memset(model->vertices, 0, sizeof(vec4) * model->modelLen->verticesMaxCount);
+
+    model->normals = malloc(sizeof(vec4) * model->modelLen->normalsMaxCount);
+    memset(model->normals, 0, sizeof(vec4) * model->modelLen->normalsMaxCount);
+
+    model->texCoords = malloc(sizeof(vec4) * model->modelLen->texCoordsMaxCount);
+    memset(model->texCoords, 0, sizeof(vec4) * model->modelLen->texCoordsMaxCount);
+
+    model->faces = malloc(sizeof(modelFace_t**) * model->modelLen->facesMaxCount);
+    memset(model->faces, 0, sizeof(modelFace_t**) * model->modelLen->facesMaxCount);
+
+    model->groupNames = malloc(sizeof(char*) * model->modelLen->groupNamesMaxCount);
+    memset(model->groupNames, 0, sizeof(char*) * model->modelLen->groupNamesMaxCount);
+
+    model->mtlLibs = malloc(sizeof(char*) * model->modelLen->mtlLibsMaxCount);
+    memset(model->mtlLibs, 0, sizeof(char*) * model->modelLen->mtlLibsMaxCount);
+
+    model->usedMaterials = malloc(sizeof(material_t*) * model->modelLen->usedMaterialsMaxCount);
+    memset(model->usedMaterials, 0, sizeof(material_t*) * model->modelLen->usedMaterialsMaxCount);
+
+    return model;
 }
 
 model_t* m_load(const char* filename)
@@ -71,20 +205,8 @@ model_t* m_load(const char* filename)
 
     fread(data, size, 1, f);
 
-    model_t* model = malloc(sizeof(model_t));
-
-    bool comment = false;
-    bool firstSymbol = false;
-
-    size_t resPos = 0, pos = 0;
-    while(resPos <= size - 1)
-    {
-        readToSeparator(data, pos, &resPos);
-        for (size_t i = pos; i < resPos; i++)
-            putchar(data[i]);
-        puts("===");
-        pos = resPos;
-    }
+    model_t* model = m_create();
+    getIndexes(data);
 
     free(data);
     fclose(f);
--- lib/resources/model.h
+++ lib/resources/model.h
@@ -12,17 +12,67 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+
 #include "GL/gl.h"
+#include "../graphics/gmath.h"
+#include "../graphics/material.h"
 
-typedef struct {
+struct _model;
 
-};
+typedef struct _modelFace {
+    struct _model* parent;
+
+    uint16_t matID;
+    uint16_t smID;
+    uint32_t groupFlags;
+
+    size_t* vertID;
+    size_t* texID;
+    size_t* normalID;
+
+} modelFace_t;
+
+#define START_LEN_COUNT 10
+#define INCREASE_LEN 1.5
+
+typedef struct _modelLen {
+    size_t verticesCount;
+    size_t normalsCount;
+    size_t texCoordsCount;
+    size_t facesCount;
+    size_t groupNamesCount;
+    size_t mtlLibsCount;
+    size_t usedMaterialsCount;
+    size_t verticesMaxCount;
+    size_t normalsMaxCount;
+    size_t texCoordsMaxCount;
+    size_t facesMaxCount;
+    size_t groupNamesMaxCount;
+    size_t mtlLibsMaxCount;
+    size_t usedMaterialsMaxCount;
+
+} modelLen_t;
 
 typedef struct _model {
+    vec4* vertices;
+    vec4* normals;
+    vec4* texCoords;
 
+    modelFace_t** faces;
+
+    char* objectName;
+    char** groupNames;
+
+    char** mtlLibs;
+    material_t** usedMaterials;
+
+    modelLen_t* modelLen;
 
 } model_t;
 
+model_t* m_create();
 model_t* m_load(const char* filename);
 void m_free(model_t* model);
 
